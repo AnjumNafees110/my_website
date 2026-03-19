fc48f05f19649272844a2723b5cf23be8b3b875f
Mchowdhury1979 <mchowpoplar@gmail.com>
2026-03-19 10:37:53 +0000

    Changes by Mohammad Chowdhury

--- Destination.py
+++ Destination.py
@@ -1,7 +1,7 @@
 destinations = ['London', 'Paris', 'Dubai']
 
 for city in destinations:
-    if city == "London":
+    if city == "New york":
         print(f"Now boarding for: {city}")
 
 print("Hi, This file is chnanged")
