c15a38b4c33af87e6c57e906d386462cbc2d27e3
Momodou Bah <moedubah@gmail.com>
2026-03-19 11:00:59 +0000

    Added a new city named 'Kingston'

--- Destination.py
+++ Destination.py
@@ -1,7 +1,7 @@
-destinations = ['London', 'Paris', 'Dubai']
+destinations = ['London', 'Paris', 'Dubai' 'Kingston']
 
 for city in destinations:
-    if city == "London":
+    if city == "kingston":
         print(f"Now boarding for: {city}")
 
 print("Hi, This file is chnanged")
