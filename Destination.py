fd83b579b720b9355dc43edc917f4bcee7d3306b
hekim01 <taslima.hekim@cancer.org.uk>
2026-03-19 09:58:27 +0000

    updated destination file

--- Destination.py
+++ Destination.py
@@ -1,7 +1,7 @@
-destinations = ['London', 'Paris', 'Dubai']
+destinations = ['Palestine', 'Paris', 'Dubai']
 
 for city in destinations:
-    if city == "London":
+    if city == "Palestine":
         print(f"Now boarding for: {city}")
 
 print("Hi, This file is chnanged")
